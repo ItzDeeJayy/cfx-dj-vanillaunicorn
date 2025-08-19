e479db921cd151b4e6c940186a5c3099e35fadb5
DJ <136165865+ItzDeeJayy@users.noreply.github.com>
2025-08-18 19:17:41 -0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'DJs Collections | https://discord.gg/TvEBNmugFp'
+description 'DJs Collections - Vanilla Unicorn'
+version '1.0.0'
+
+lua54 'yes'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/ytyp/vu.ytyp'
+
+this_is_a_map 'yes'
+
+dependencies 'alca_anim_eat' -- FREE
+
+-- You need either one of the animations to use our premade configs
+
+--dependencies 'cfx-djs-anims' -- Paid
+
+-- (need to make ytd)
